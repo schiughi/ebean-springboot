4fa60f8949616feca84031aea8575aba0a5c2255
schiughi <schiughi@gmail.com>
2017-12-24 18:04:12 +0900

    Isbnのvalidationを追加
    テストはまだ

--- src/main/kotlin/com/schiughi/esboot/domain/entities/book/Isbn.kt
+++ src/main/kotlin/com/schiughi/esboot/domain/entities/book/Isbn.kt
@@ -1,7 +1,13 @@
 package com.schiughi.esboot.domain.entities.book
 
 import com.schiughi.esboot.domain.entities.ValueObject
+import javax.validation.constraints.Pattern
+import javax.validation.constraints.Size
 
-data class Isbn(val value: String = "") : ValueObject{
+data class Isbn(
+        @Size(min = 13, max = 17)
+        @Pattern(regexp = "([0-9]{3})-?([0-9]+)-?([0-9]+)-?([0-9]+)-?([0-9])")
+        private val value: String
+) : ValueObject {
     override fun toString() = value
 }
